b373fb82e31f859c67f013451391ef3d6a81dedf
NiteHawk <n1tehawk@users.noreply.github.com>
2015-11-21 19:41:50 +0100

    Be more consistent in naming the package: LuaXml -> LuaXML
    Signed-off-by: NiteHawk <n1tehawk@users.noreply.github.com>

--- test.lua
+++ test.lua
@@ -1,12 +1,12 @@
-xml = require('LuaXml')
+xml = require('LuaXML')
 
 -- load XML data from file "test.xml" into local table xfile
 local xfile = xml.load("test.xml")
 -- search for substatement having the tag "scene"
 local xscene = xfile:find("scene")
--- if this substatement is found…
+-- if this substatement is found...
 if xscene ~= nil then
-  --  …print it to screen
+  --  ...print it to screen
   print(xscene)
   --  print  tag, attribute id and first substatement
   print( xscene:tag(), xscene.id, xscene[1] )
