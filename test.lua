aace8cadba3681bf57826988dc6d724e86f57694
NiteHawk <n1tehawk@users.noreply.github.com>
2015-11-26 09:31:29 +0100

    Introduce new iterate() and children() functions, rework find()
    Signed-off-by: NiteHawk <n1tehawk@users.noreply.github.com>

--- test.lua
+++ test.lua
@@ -12,5 +12,24 @@ if xscene ~= nil then
   print( xscene:tag(), xscene.id, xscene[1] )
 end
 
+-- demonstrate iterating the "resources" children with a certain MIME type
+for k, v in xfile:find("resources"):children(nil, "mime", "model/vrml") do
+	print((string.gsub(v:str(), "\n", "")))
+end
+-- since the attribute match is unambiguous, we can even use xfile recursively
+for k, v in xfile:children(nil, "mime", "image/png", true) do
+	print(v:tag(), v.name, v.id, v.mime, v.url)
+end
+
+-- demonstrate a more complex callback: iterate() over all elements having
+-- an "id" attribute, and show those where it starts with "fa" or "fg"
+xfile:iterate(
+	function(var, depth)
+		if var.id:match("^f[ag]") then
+			print(var:tag(), var.id, var[1])
+		end
+	end,
+nil, "id", nil, true)
+
 xfile:save"t.xml"
 print("---\nREADY.")
